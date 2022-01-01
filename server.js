7cc8777420d3a1bde298c53d98204b54cd45c5db
mohamedelsherif1997 <mohamed.elsherif.1997@gmail.com>
2022-01-01 20:11:20 +0200

    This is a commit of server.js file

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+//global constants 
+const hostName = "127.0.0.1";
+const port = 5500;
+
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
@@ -12,15 +16,36 @@ const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 const cors = require('cors');
+
 app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// function to get data from site . 
+function getData (req,res){
+    res.send(projectData);
+};
+// Te route
+app.get(`/all`, getData);
 
-// Setup Server
+// function to post data 
+function postData (req,res){
+    projectData = req.body;
+    res.send (projectData);
+}
+app.post('/add', postData);
+
+//Testion the server
+
+function testServer (){
+    console.log(`The server is working at https://${hostName} and Port : ${port}`);
+}
+
+app.listen(port,testServer());
